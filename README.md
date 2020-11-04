5388f87330cf20c9db8aa31364069d84e3641161
Germandf <germandefrancesco@hotmail.com>
2020-11-04 19:17:34 -0300

    Dessert interface and dessert-list component's template creation

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Install dependences
+
+Run `npm install` to get all the dependences that are not in github (in this case the Angular framework)
--- src/app/dessert-list/dessert-list.component.ts
+++ src/app/dessert-list/dessert-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dessert } from './Dessert';
 
 @Component({
   selector: 'app-dessert-list',
@@ -7,13 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DessertListComponent implements OnInit {
 
-  dessert = {
-    "name": "Brownie Tradicional",
-    "price": "90",
-    "price2": "160",
-    "stock": "10",
-    "image": "assets/images/1.jpeg",
-  };
+  desserts : Dessert[] = [
+    {
+      "name": "Brownie Tradicional",
+      "price": 90,
+      "price2": 160,
+      "stock": 10,
+      "image": "assets/images/1.jpeg",
+      "hasDiscount": false,
+    },
+    {
+      "name": "Mini Torta Brownie con nueces",
+      "price": 180,
+      "price2": 340,
+      "stock": 20,
+      "image": "assets/images/2.jpeg",
+      "hasDiscount": true,
+    },
+    {
+      "name": "Mini Lemon Pie",
+      "price": 130,
+      "price2": 250,
+      "stock": 15,
+      "image": "assets/images/3.jpeg",
+      "hasDiscount": false,
+    },
+    {
+      "name": "Tarta Tofi",
+      "price": 130,
+      "price2": 250,
+      "stock": 0,
+      "image": "assets/images/4.jpeg",
+      "hasDiscount": false,
+    },
+  ];
 
   constructor() { }
 
